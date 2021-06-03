e1012275775e51c3fa116d456059d073d95bc4a0
Vavish <Violeta@MacBook-de-Vavish-2.local>
2021-06-03 17:56:58 -0500

    Pruebas

---
+++ arreglo/arreglo.js
@@ -0,0 +1,4 @@
+arreglo_numeros = [1,2,3,60,4,5,6,7,50,9,10,30];
+
+    console.log(Math.max.apply(null,arreglo_numeros)); //clase+metodo+invocacion+argumentos
+
---
+++ variables/variables.js
@@ -0,0 +1,5 @@
+var nombre = "Violeta Torres";
+console.log (nombre);
+
+var numero =14;
+console.log(numero);
